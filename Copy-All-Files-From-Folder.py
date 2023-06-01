c39c80124a13dbf04b1bcb03e67be74b11e53d09
Cedric Boucher <onebitalpha@gmail.com>
2023-05-31 22:22:26 -0600

    bug fix

--- Copy-All-Files-From-Folder.py
+++ Copy-All-Files-From-Folder.py
@@ -165,6 +165,7 @@ def move_file_error(source_file_path: str, destination_folder: str, filename: st
             # we assume they are the same file, so we can move the source file to trash
             # and keep the destination file where it is
             send2trash(source_file_path)
+            return True
     
     else:
         # for now I don't know what else the error could be
