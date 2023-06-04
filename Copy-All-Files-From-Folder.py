4cf0bb9ac99a8f7bdf4b6b07ae631758158d01c0
Cedric Boucher <onebitalpha@gmail.com>
2023-06-04 16:41:02 -0600

    converted progress bar to a class

--- Copy-All-Files-From-Folder.py
+++ Copy-All-Files-From-Folder.py
@@ -59,36 +59,58 @@ def get_num_files_in_folder(path, file_extensions: tuple[str] = (), start_with: 
     return num_files
 
 
-def progress_bar(progress: float, length: int, start_string = "~<{", end_string = "}>~", fill_char = "/", empty_char = "-", overwrite_previous_bar = True, with_percentage = True) -> None:
+class progress_bar:
     """
     progress is float [0, 1]
     length is character count length of progress bar (not including start and end strings)
     no newline is printed after the progress bar
     """
-    assert (type(progress) == float), "type of progress was not float"
-    assert (type(length) == int), "type of length was not int"
-    assert (progress <= 1), "progress was greater than 1"
-    assert (length > 0), "length was negative"
-    assert (type(start_string) == str), "start_string was not string"
-    assert (type(end_string) == str), "end_string was not string"
-    assert (type(fill_char) == str), "fill_char was not string"
-    assert (len(fill_char) == 1), "len(fill_char) was not 1"
-    assert (type(empty_char) == str), "empty_char was not string"
-    assert (len(empty_char) == 1), "len(empty_char) was not 1"
+    def __init__(self, length: int, start_string = "~<{", end_string = "}>~", fill_char = "/", empty_char = "-", with_percentage = True, with_ETA = True):
+        assert (type(length) == int), "type of length was not int"
+        assert (length > 0), "length was negative"
+        assert (type(start_string) == str), "start_string was not string"
+        assert (type(end_string) == str), "end_string was not string"
+        assert (type(fill_char) == str), "fill_char was not string"
+        assert (len(fill_char) == 1), "len(fill_char) was not 1"
+        assert (type(empty_char) == str), "empty_char was not string"
+        assert (len(empty_char) == 1), "len(empty_char) was not 1"
 
-    output_string = start_string
-    progress_character_count = int(progress*length)
-    output_string += progress_character_count * fill_char
-    output_string += (length - progress_character_count) * empty_char
-    output_string += end_string
-    if overwrite_previous_bar:
-        output_string = "\r" + output_string
-        print(" " * (length + len(start_string) + len(end_string)), end="")
-    print(output_string, end="")
-    if with_percentage:
-        print(" {:6.2f}%".format(progress*100), end="")
+        self.__length = length
+        self.__start_string = start_string
+        self.__end_string = end_string
+        self.__fill_char = fill_char
+        self.__empty_char = empty_char
+        self.__with_percentage = with_percentage
+        self.__with_ETA = with_ETA
+        self.__ETA = None # don't start the ETA stopwatch until first update
 
-    return None
+        self.__output_string = "" # not defined yet
+
+        return None
+
+
+    def __update_output_string(self, progress: float) -> None:
+        assert (type(progress) == float), "type of progress was not float"
+        assert (progress <= 1), "progress was greater than 1"
+
+        output_string = self.__start_string
+        progress_character_count = int(progress*self.__length)
+        output_string += progress_character_count * self.__fill_char
+        output_string += (self.__length - progress_character_count) * self.__empty_char
+        output_string += self.__end_string
+        if self.__with_percentage:
+            output_string += " {:6.2f}%".format(progress*100)
+        if self.__with_ETA:
+            if self.__ETA is None:
+                self.__ETA = ETA() # start ETA stopwatch on first update
+            output_string += " {} remaining".format(seconds_to_time(self.__ETA.get_time_remaining(progress)))
+
+        self.__output_string = output_string
+        return None
+    
+    def print_progress_bar(self, progress) -> None:
+        self.__update_output_string(progress)
+        print("\r" + self.__output_string, end="")
 
 
 def move_files(input_folder, output_folder = None, file_extensions: tuple[str] = (), start_with: tuple[str] = (), move_mode: str = "C") -> int:
@@ -242,8 +264,9 @@ class ETA:
     def __init__(self):
         self.__start_time = time()
         self.__time_remaining = -1 # not defined yet
-    
-    def __update(self, progress: float):
+        return None
+
+    def __update(self, progress: float) -> None:
         """
         updates self.time_remaining
         progress should be float [0, 1]
@@ -253,13 +276,30 @@ class ETA:
         time_delta = (time() - self.__start_time)
         estimated_total_time = (time_delta / progress)
         self.__time_remaining = (estimated_total_time - time_delta)
+        return None
 
-    def get_time_remaining(self):
+    def get_time_remaining(self, progress) -> float:
         """
         getter for self.time_remaining
         """
-        self.__update()
+        self.__update(progress)
         return self.__time_remaining
+
+
+def seconds_to_time(seconds: float) -> str:
+    """
+    converts seconds into string:
+    {:2d} years, {:3d} days, {:2d} hours, {:2d} minutes, {:6.2f} seconds
+    """
+    seconds = time%60 # remainder after removing seconds above 60
+    minutes = int(time//60%60) # one minute is 60 seconds, so mod the time by seconds in a minute, then take the remainder after removing minutes above 60
+    hours = int(time//3600%24) # one hour is 3600 seconds, so mod the time by seconds in an hour, then take the remainder after removing hours above 24
+    days = int(time//86400%365) # one day is 86400 seconds, so mod the time by seconds in a day, then take the remainder after removing days above 365
+    years = int(time//31536000) # one year is 31536000 seconds, so mod the time by seconds in a year
+
+    output_string = "{:2d} years, {:3d} days, {:2d} hours, {:2d} minutes, {:6.2f} seconds".format(years, days, hours, minutes, seconds)
+
+    return output_string
 
 
 def main() -> None:
