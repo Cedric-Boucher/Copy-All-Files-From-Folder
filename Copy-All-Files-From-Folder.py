0234f09101c0611033bcf3e10b869dc4f4d3ad89
Cedric Boucher <onebitalpha@gmail.com>
2023-06-04 16:51:27 -0600

    bug fix

--- Copy-All-Files-From-Folder.py
+++ Copy-All-Files-From-Folder.py
@@ -288,16 +288,16 @@ class ETA:
         return self.__time_remaining
 
 
-def seconds_to_time(seconds: float) -> str:
+def seconds_to_time(secs: float) -> str:
     """
     converts seconds into string:
     {:2d} years, {:3d} days, {:2d} hours, {:2d} minutes, {:6.2f} seconds
     """
-    seconds = time%60 # remainder after removing seconds above 60
-    minutes = int(time//60%60) # one minute is 60 seconds, so mod the time by seconds in a minute, then take the remainder after removing minutes above 60
-    hours = int(time//3600%24) # one hour is 3600 seconds, so mod the time by seconds in an hour, then take the remainder after removing hours above 24
-    days = int(time//86400%365) # one day is 86400 seconds, so mod the time by seconds in a day, then take the remainder after removing days above 365
-    years = int(time//31536000) # one year is 31536000 seconds, so mod the time by seconds in a year
+    seconds = secs%60 # remainder after removing seconds above 60
+    minutes = int(secs//60%60) # one minute is 60 seconds, so mod the time by seconds in a minute, then take the remainder after removing minutes above 60
+    hours = int(secs//3600%24) # one hour is 3600 seconds, so mod the time by seconds in an hour, then take the remainder after removing hours above 24
+    days = int(secs//86400%365) # one day is 86400 seconds, so mod the time by seconds in a day, then take the remainder after removing days above 365
+    years = int(secs//31536000) # one year is 31536000 seconds, so mod the time by seconds in a year
 
     output_string = "{:2d} years, {:3d} days, {:2d} hours, {:2d} minutes, {:6.2f} seconds".format(years, days, hours, minutes, seconds)
 
