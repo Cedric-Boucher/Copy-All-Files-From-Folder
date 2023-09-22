a351ad1790218c37d4a3f90fb3f01b3ef923a1a4
Cedric Boucher <onebitalpha@gmail.com>
2023-09-21 20:54:34 -0600

    can now get a filelist

--- Filelist.py
+++ Filelist.py
@@ -15,8 +15,23 @@ class Filelist():
         assert (isinstance(max_filesize, int)), "max_filesize was not an integer"
         assert (max_filesize >= min_filesize), "max_filesize was not greater than or equal to min_filesize"
 
-        self.__input_folder = input_folder
-        self.__file_extensions = file_extensions
-        self.__start_with = start_with
-        self.__min_filesize = min_filesize
-        self.__max_filesize = max_filesize
+        self.__input_folder = os.path.abspath(input_folder)
+        self.__file_extensions: tuple[str] = file_extensions
+        self.__start_with: tuple[str] = start_with
+        self.__min_filesize: int = min_filesize
+        self.__max_filesize: int = max_filesize
+
+        self.__filepaths: tuple[str] = tuple()
+        self.__get_files()
+
+    def __get_files(self) -> None:
+        """
+        populates self.__filepaths
+        """
+        files = list()
+
+        for path_to_file, _, sub_files in os.walk(self.__input_folder):
+            files.extend([os.path.abspath(path_to_file+"/"+sub_file) for sub_file in sub_files])
+
+        self.__filepaths = tuple(files)
+
